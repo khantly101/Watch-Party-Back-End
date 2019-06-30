74936a7563e0776522cabe04ab17743291225022
Mujtaba Badar <mujbadar@macbook-pro.lan>
2019-06-30 15:01:06 -0500

    fixed typos

--- controllers/newUsers.js
+++ controllers/newUsers.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt')
 //Create new user
 router.post('/', (req, res) => {
   //Password encryption
-  req.body.passord = bcrypt.hashsync(req.body.password, bcrypt.gensaltsync(10))
+  req.body.password = bcrypt.hashsync(req.body.password, bcrypt.gensaltsync(10))
   User.create(req.body, (error, newUser) => {
     if (error) {
       res.status(400).json({error: error.message})
---
+++ models/partyRoom.js
@@ -0,0 +1,13 @@
+const mongoose = require(`mongoose`)
+const Schema = mongoose.Schema
+
+const partyroomSchema = new Schema({
+  roomName: { type: String, require: true },
+  nameSpace: { type: String, require: true },
+  creator: { type: String, require: true },
+  img: String,
+  description: String
+})
+
+const PartyRoom = mongoose.model(`PartyRoom`, partyroomSchema)
+module.exports = PartyRoom
