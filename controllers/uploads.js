48df2f3cb6dc9a8dfefe116f1177049938338411
Mujtaba Badar <mujbadar@macbook-pro.lan>
2019-07-05 19:15:54 -0500

    Aws image upload

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -3,22 +3,42 @@ const router = express.Router()
 
 const Upload = require('../models/upload.js')
 const PartyRoom = require('../models/partyRoom.js')
+const User = require('../models/user.js')
 
 
-const singleUpload = Upload.single('video')
+const upload = Upload.single('upload')
+
 
 router.post('/:id', (req, res) => {
-  singleUpload(req, res, (err) => {
+  upload(req, res, (err) => {
     console.log(req.file.location);
-    PartyRoom.findByIdAndUpdate(req.params.id, {upload: req.file.location}, {new: true}, (error, pushed) => {
-      if (error) {
-        console.log('push failed');
-      }else {
-        console.log(pushed);
-      }
-    })
-    return res.json({'videoUrl': req.file.location})
+    console.log(req.file);
+    if (req.file.mimetype === 'video/mp4') {
+      PartyRoom.findByIdAndUpdate(req.params.id, {upload: req.file.location}, {new: true}, (error, pushed) => {
+        if (error) {
+          console.log('push failed');
+        }else {
+          console.log(pushed);
+        }
+      })
+      return res.json({'videoUrl': req.file.location})
+    } else {
+      User.findByIdAndUpdate(req.params.id, {img: req.file.location}, {new:true}, (error, pushed) => {
+        if (error) {
+          console.log('push failed');
+        } else {
+          console.log(pushed);
+        }
+      })
+    }
+
   })
 })
+
+// router.post('/photo', (req, res) => {
+//   photoUpload(req, res, (err) => {
+//     return res.json()
+//   })
+// })
 
 module.exports = router
